055ffb012eb327eee6b8489de4629aeaa45ca842
Michael Nissen <hi@michaelnissen.co>
2024-07-20 15:02:57 +0100

    feat: create helper function to execute and type cast a factory

--- create.go
+++ create.go
@@ -13,8 +13,17 @@ type Factory func() (AnyFactory, DisposeFn)
 type Factories map[string]Factory
 
 type Context struct {
-	factories Factories
+	factories   Factories
 	disposables []DisposeFn
+}
+
+func Exec[T any](factory *Factory) (*T, DisposeFn, error) {
+	value, dispose := (*factory)()
+	tf, ok := value.(T); if !ok {
+		return nil, nil, fmt.Errorf("Failed to cast factory %s to type %T", value, *new(T))
+	}
+
+	return &tf, dispose, nil
 }
 
 func Resolve[T any](ctx *Context, name string) *T {
@@ -22,26 +31,25 @@ func Resolve[T any](ctx *Context, name string) *T {
 		panic(fmt.Errorf("No factory found: %s", name))
 	}
 
-	value, dispose := _factory()
-
-	factory, ok := value.(T); if !ok {
-		panic(fmt.Sprintf("Failed to cast factory %s to type %T", value, *new(T)))
+	factory, dispose, err := Exec[T](&_factory); if err != nil {
+		panic(err)
 	}
 
 	ctx.disposables = append(ctx.disposables, dispose)
-	return &factory
+	return factory
 }
 
 func ResolveOr[T any](ctx *Context, name string) (*T, error) {
 	_factory, ok := ctx.factories[name]; if !ok {
 		return nil, fmt.Errorf("No factory found: %s", name)
 	}
-	value, dispose := _factory()
-	factory, ok := value.(T); if !ok {
-		return nil, fmt.Errorf("Failed to cast factory %s to type %T", value, *new(T))
+
+	factory, dispose, err := Exec[T](&_factory); if err != nil {
+		return nil, err
 	}
+
 	ctx.disposables = append(ctx.disposables, dispose)
-	return &factory, nil
+	return factory, nil
 }
 
 func (c *Context) Dispose() {
@@ -51,14 +59,14 @@ func (c *Context) Dispose() {
 func CreateContext(factories Factories) *Context {
 	return &Context{
 		factories: factories,
-		disposables: make([]DisposeFn, 0), 
+		disposables: make([]DisposeFn, 0),
 	}
 }
 
 func Shared(factory Factory) Factory {
 	var (
 		instance AnyFactory
-		dispose DisposeFn
+		dispose  DisposeFn
 		once sync.Once
 		mutex sync.Mutex
 	)
@@ -75,4 +83,3 @@ func Shared(factory Factory) Factory {
 		return instance, dispose
 	}
 }
-
